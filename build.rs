e3737d050eaf7dd1e77f199e51b0bab161751f84
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-01-11 18:43:45 -0500

    Fix linking

--- build.rs
+++ build.rs
@@ -7,15 +7,15 @@ fn main() {
     println!("cargo:rustc-link-search=native=./fsr2/vk");
     println!("cargo:rustc-link-lib=static=ffx_fsr2_api_x86_64");
     println!("cargo:rustc-link-lib=static=ffx_fsr2_api_vk_x86_64");
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+    println!("cargo:rustc-link-lib=dylib=vulkan");
 
     let bindings = Builder::default()
         .header("fsr2/ffx_fsr2.h")
         .header("fsr2/vk/ffx_fsr2_vk.h")
         .clang_args(["-x", "c++", "-fdeclspec"])
-        .allowlist_type("FfxFsr2.*")
-        .allowlist_function("ffxFsr2.*")
-        .blocklist_type("Vk.*")
-        .blocklist_type(".*vk.*")
+        .blocklist_type("VkPhysicalDevice")
+        .blocklist_type("PFN_vkGetDeviceProcAddr")
         .generate()
         .unwrap();
 
