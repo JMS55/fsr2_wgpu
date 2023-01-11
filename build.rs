eb7895c91bb5acf7b71e3749ca73cc041953b66a
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-01-10 23:43:07 -0500

    Basic setup

---
+++ build.rs
@@ -0,0 +1,21 @@
+use bindgen::Builder;
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-search=native=fsr2");
+    println!("cargo:rustc-link-lib=libffx_fsr2_api_x86_64");
+    println!("cargo:rustc-link-lib=vk/libffx_fsr2_api_vk_x86_64");
+
+    let bindings = Builder::default()
+        .header("fsr2/ffx_fsr2.h")
+        .header("fsr2/vk/ffx_fsr2_vk.h")
+        .clang_args(["-x", "c++", "-fdeclspec"])
+        .generate()
+        .unwrap();
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .unwrap();
+}
