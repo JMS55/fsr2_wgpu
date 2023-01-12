34a622cc1b0c6b4689efa660d6aa8cac48b54ca7
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-01-11 20:06:21 -0500

    Add debug fsr2 libs

--- build.rs
+++ build.rs
@@ -3,16 +3,15 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rustc-link-search=native=./fsr2");
-    println!("cargo:rustc-link-search=native=./fsr2/vk");
-    println!("cargo:rustc-link-lib=static=ffx_fsr2_api_x86_64");
-    println!("cargo:rustc-link-lib=static=ffx_fsr2_api_vk_x86_64");
+    println!("cargo:rustc-link-search=native=./fsr2/lib");
+    println!("cargo:rustc-link-lib=static=ffx_fsr2_api_x86_64d");
+    println!("cargo:rustc-link-lib=static=ffx_fsr2_api_vk_x86_64d");
     println!("cargo:rustc-link-lib=dylib=stdc++");
     println!("cargo:rustc-link-lib=dylib=vulkan");
 
     let bindings = Builder::default()
-        .header("fsr2/ffx_fsr2.h")
-        .header("fsr2/vk/ffx_fsr2_vk.h")
+        .header("fsr2/include/ffx_fsr2.h")
+        .header("fsr2/include/vk/ffx_fsr2_vk.h")
         .clang_args(["-x", "c++", "-fdeclspec"])
         .blocklist_type("VkPhysicalDevice")
         .blocklist_type("PFN_vkGetDeviceProcAddr")
