df0bdf62e4ba6d0b5d2d2f9b46207c7e2dfea75e
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-01-17 14:22:43 -0500

    More WIP barriers

--- src/barrier.rs
+++ src/barrier.rs
@@ -1,47 +1,80 @@
 use arrayvec::ArrayVec;
-use ash::vk::{CommandBuffer, DependencyFlags, Image, ImageMemoryBarrier, PipelineStageFlags};
+use ash::vk::{
+    AccessFlags, CommandBuffer, DependencyFlags, Image, ImageLayout, ImageMemoryBarrier,
+    PipelineStageFlags, StructureType, QUEUE_FAMILY_IGNORED,
+};
+use std::ptr;
 use wgpu::Device;
 use wgpu_core::api::Vulkan;
 
 #[derive(Default)]
 pub struct Barriers {
-    start_pipeline_stage: PipelineStageFlags,
+    current_pipeline_stage: PipelineStageFlags,
     start_barriers: ArrayVec<ImageMemoryBarrier, 7>,
     end_barriers: ArrayVec<ImageMemoryBarrier, 7>,
 }
 
 impl Barriers {
     pub fn add(&mut self, image: Image) {
-        todo!()
+        let current_access_mask = todo!();
+        let current_layout = todo!();
+        let subresource_range = todo!();
+        self.current_pipeline_stage |= todo!();
+
+        self.start_barriers.push(ImageMemoryBarrier {
+            s_type: StructureType::IMAGE_MEMORY_BARRIER,
+            p_next: ptr::null(),
+            src_access_mask: current_access_mask,
+            dst_access_mask: AccessFlags::SHADER_READ,
+            old_layout: current_layout,
+            new_layout: ImageLayout::READ_ONLY_OPTIMAL,
+            src_queue_family_index: QUEUE_FAMILY_IGNORED,
+            dst_queue_family_index: QUEUE_FAMILY_IGNORED,
+            image,
+            subresource_range,
+        });
+
+        self.end_barriers.push(ImageMemoryBarrier {
+            s_type: StructureType::IMAGE_MEMORY_BARRIER,
+            p_next: ptr::null(),
+            dst_access_mask: current_access_mask,
+            src_access_mask: AccessFlags::SHADER_READ,
+            old_layout: ImageLayout::READ_ONLY_OPTIMAL,
+            new_layout: current_layout,
+            src_queue_family_index: QUEUE_FAMILY_IGNORED,
+            dst_queue_family_index: QUEUE_FAMILY_IGNORED,
+            image,
+            subresource_range,
+        });
     }
 
     // TODO: Get Device from CommandEncoder instead
 
     pub unsafe fn cmd_start(&self, command_buffer: CommandBuffer, device: &Device) {
-            device.as_hal::<Vulkan, _, _>(|device| {
-                device.unwrap().raw_device().cmd_pipeline_barrier(
-                    command_buffer,
-                    self.start_pipeline_stage,
-                    PipelineStageFlags::COMPUTE_SHADER,
-                    DependencyFlags::empty(),
-                    &[],
-                    &[],
-                    &self.start_barriers,
-                )
-            });
+        device.as_hal::<Vulkan, _, _>(|device| {
+            device.unwrap().raw_device().cmd_pipeline_barrier(
+                command_buffer,
+                self.current_pipeline_stage,
+                PipelineStageFlags::COMPUTE_SHADER,
+                DependencyFlags::empty(),
+                &[],
+                &[],
+                &self.start_barriers,
+            )
+        });
     }
 
     pub unsafe fn cmd_end(&self, command_buffer: CommandBuffer, device: &Device) {
-            device.as_hal::<Vulkan, _, _>(|device| {
-                device.unwrap().raw_device().cmd_pipeline_barrier(
-                    command_buffer,
-                    PipelineStageFlags::COMPUTE_SHADER,
-                    self.start_pipeline_stage,
-                    DependencyFlags::empty(),
-                    &[],
-                    &[],
-                    &self.end_barriers,
-                )
-            });
+        device.as_hal::<Vulkan, _, _>(|device| {
+            device.unwrap().raw_device().cmd_pipeline_barrier(
+                command_buffer,
+                PipelineStageFlags::COMPUTE_SHADER,
+                self.current_pipeline_stage,
+                DependencyFlags::empty(),
+                &[],
+                &[],
+                &self.end_barriers,
+            )
+        });
     }
 }
